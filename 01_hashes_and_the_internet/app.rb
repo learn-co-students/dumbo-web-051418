7d7c65938b43999bfbab9d7838341e83589926df
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-05-15 15:53:55 -0400

    Update to get an example of building a cool app

--- 01_hashes_and_the_internet/app.rb
+++ 01_hashes_and_the_internet/app.rb
@@ -13,15 +13,52 @@ require 'pry'
 
 # The Base URL: https://www.googleapis.com/books/v1/volumes?q={}
 
-def search_book(term)
-  response = RestClient.get "https://www.googleapis.com/books/v1/volumes?q=#{term}"
-
-  # Find books off of a particular search term
-  # List out the book's title, author, and publishDate
-  binding.pry
+def welcome
+  puts "Welcome to Google Books Finder!"
 end
 
-search_book("cats")
+def get_user_input
+  puts "What term would you like to look for?"
+  gets.chomp
+end
+
+def search_for_term(term)
+  response = RestClient.get "https://www.googleapis.com/books/v1/volumes?q=#{term}"
+  json = JSON.parse(response)
+
+  json
+end
+
+def book_info(json)
+  json["items"].map do |book|
+    {
+      title: book["volumeInfo"]["title"],
+      publisher: book["volumeInfo"]["publisher"],
+      publishedDate: book["volumeInfo"]["publishedDate"]
+    }
+  end
+end
+
+def display_books(books)
+  books.each do |book|
+    puts "*" * 8
+    puts "Title: #{book[:title]}"
+    puts "Publisher: #{book[:publisher]}"
+    puts "Published Date: #{book[:publishedDate]}"
+  end
+end
+
+
+def run
+  welcome                             # Here we put the welcome message
+  search_term = get_user_input        # Ask our user for what they want to look for
+  json = search_for_term(search_term) # Make a request to the API to get data
+  books = book_info(json)             # Organize to display book's title, book's publisher, and publishing date
+
+  display_books(books)                # Show us info about the books
+end
+
+run                                   # Finally to run it all
 
 
 #RestClient.get 'https://www.reddit.com/.json'
