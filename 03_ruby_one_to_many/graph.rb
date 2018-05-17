b6f8e246727ee0c44dc6b7bd6d3f237f1b6caa51
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-05-17 11:26:46 -0400

    Add mid-lecture code

--- 03_ruby_one_to_many/graph.rb
+++ 03_ruby_one_to_many/graph.rb
@@ -1,1 +1,20 @@
+# Attribute accessors
+class Graph
+  attr_accessor :x_bound, :y_bound
 
+  def initialize(x, y)
+    @x_bound = x
+    @y_bound = y
+  end
+
+  def add_shape(new_shape)
+    new_shape.plot(self)
+    # Adding a shape that exists to my graph
+  end
+
+  def shapes
+    Shape.all.select do |shape|
+      shape.graph == self
+    end
+  end
+end
--- 03_ruby_one_to_many/shape.rb
+++ 03_ruby_one_to_many/shape.rb
@@ -1,1 +1,29 @@
+class Shape
+  # area, length, width, perimeter
+  attr_reader :graph
 
+  @@all = []
+
+  def initialize(length, width)
+    @length = length
+    @width = width
+
+    @@all << self
+  end
+
+  def plot(my_graph)
+    @graph = my_graph
+  end
+
+  def self.all
+    @@all
+  end
+
+  def area
+    # Calculates area from attributes (length, width)
+  end
+
+  def perimeter
+    # Calculates perimeter from attributes (length, width)
+  end
+end
