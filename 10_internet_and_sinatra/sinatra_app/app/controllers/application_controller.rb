6157b954ae1f83f145a5ebf3975e1389787ba969
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-06-04 16:01:10 -0400

    Add the last bit of code

--- 10_internet_and_sinatra/sinatra_app/app/controllers/application_controller.rb
+++ 10_internet_and_sinatra/sinatra_app/app/controllers/application_controller.rb
@@ -1,21 +1,51 @@
 class ApplicationController < Sinatra::Base
 
+  set :root, File.dirname(__FILE__)
+  set :views, "#{settings.root}/../views"
+
   # Go to this route and get back all
   # book resources
+  # The Index route
   get '/books' do
-    books = Book.all.map do |book|
-      "<li><a href='/books/#{book.id}'>#{book.title}</a></li>"
+    @books = Book.all
+
+    erb :'index.html'
+    # "<a href='/books/new'>Add a new book</a><ul>#{books.join}</ul>"
+  end
+
+  # Create a new book
+  get "/books/new" do
+    erb :'new.html'
+  end
+
+  post "/books/create" do
+    # book = Book.create(params)
+    book = Book.new(params)
+
+    if book.save
+      # redirect to the show page
+      redirect "/books/#{book.id}"
+    else
+
+      redirect '/books/new'
+      # redirect to the new form
     end
 
-    "<ul>#{books.join}</ul>"
+    # binding.pry
+  end
+
+  delete "/books/:id" do
+    # book.destroy(id)
+    binding.pry
+
   end
 
   # Go to this route and get back
   # specified resource for book
   get "/books/:id" do
     book_id = params["id"] # the request's id
-    book = Book.find(book_id)
+    @book = Book.find(book_id)
 
-    "<h1>#{book.title}</h1><p>#{book.snippet}</p>"
+    erb :'show.html'
   end
 end
