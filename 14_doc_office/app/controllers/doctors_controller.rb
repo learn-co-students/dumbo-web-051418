4a31497ef55d6b89272e111580aef083a8bedd87
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-06-11 14:36:48 -0400

    Add lecture code

---
+++ 14_doc_office/app/controllers/doctors_controller.rb
@@ -0,0 +1,32 @@
+class DoctorsController < ApplicationController
+  # Methods known as actions
+  # This is how our user/db talk to one another
+
+  def index
+    @doctors = Doctor.all
+    render :index
+  end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+    render :show
+  end
+
+  def new
+    @doctor = Doctor.new
+    render :new
+  end
+
+  def create
+    doctor = Doctor.create(doctor_params)
+
+    # redirect_to doctor_path(@doctor)
+    # redirect_to @doctor
+    redirect_to doctor_url(doctor)
+  end
+
+  private
+  def doctor_params
+    params.require(:doctor).permit(:name, :specialty)
+  end
+end
