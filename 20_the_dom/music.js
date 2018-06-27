34cbff1bb2a1d97058361adccc1a596e5250f5ba
nikymorg <nicole.morgan@gmail.com>
2018-06-27 07:50:29 -0400

    lecture prep

---
+++ 20_the_dom/music.js
@@ -0,0 +1,36 @@
+const nineties = [
+  {
+    name: 'Lisa Loeb',
+    bio: 'Lisa Loeb is a singer-songwriter, producer, touring artist, author and philanthropist who started her career with the platinum-selling Number 1 hit song “Stay (I Missed You)” from the film Reality Bites. A trailblazing independent artist, Lisa was the first pop musician to have a Number 1 single while not signed to a recording contract. She followed that remarkable feat with the hit singles "Do You Sleep," "I Do," and "Let\'s Forget about It” and the albums “Cake and Pie” and “No Fairy Tale,” among others.',
+    picture: 'https://s-media-cache-ak0.pinimg.com/originals/9f/7f/c9/9f7fc9ec1b76ce4bcc6a25554bd71c02.jpg'
+    video: 'https://www.youtube.com/watch?v=9Z_H11F7WDI'
+    url: 'https://www.lisaloeb.com/'
+  },
+  {
+    name: 'Natalie Imbruglia',
+    bio: 'Natalie Jane Imbruglia is an Australian singer-songwriter, model and actress, naturalised in the UK.[1] In the early 1990s, she played Beth Brennan in the Australian soap opera Neighbours. Three years after leaving the programme, she began a singing career with her successful hit cover of Ednaswap\'s song "Torn".',=
+    video: 'https://www.youtube.com/watch?v=VV1XWJN3nJo'
+    url: 'http://www.natalieimbruglia.com/'
+  },
+  {
+    name: 'The Mighty Mighty Bosstones',
+    bio: 'The Mighty Mighty Bosstones (informally referred to as The Bosstones) are an American ska punk band from Boston, Massachusetts, formed in 1983.',
+    picture: 'https://img.washingtonpost.com/blogs/reliable-source/files/2017/08/mighty.jpg'
+    video: 'https://www.youtube.com/watch?time_continue=2&v=NIGMUAMevH0'
+    url: 'http://www.bosstonesmusic.com/'
+  },
+  {
+    name: 'Third Eye Blind',
+    bio: 'Third Eye Blind is an American rock band formed in San Francisco in 1993. The songwriting duo of Stephan Jenkins and Kevin Cadogan signed the band\'s first major label recording contract with Elektra Records in 1996, which was later reported as the largest publishing deal ever for an unsigned artist.',
+    picture: 'https://s-media-cache-ak0.pinimg.com/originals/9f/7f/c9/9f7fc9ec1b76ce4bcc6a25554bd71c02.jpg'
+    video: 'https://www.youtube.com/watch?v=beINamVRGy4'
+    url: 'http://www.thirdeyeblind.com/'
+  },
+  {
+    name: 'The Cranberries',
+    bio: 'The Cranberries are an Irish rock band formed in Limerick in 1989 by lead singer Niall Quinn, guitarist Noel Hogan, bassist Mike Hogan, and drummer Fergal Lawler. Quinn was replaced as lead singer by Dolores O\'Riordan in 1990.',
+    picture: 'http://www.alternativenation.net/wp-content/uploads/2018/01/cranberriesyoung.jpg'
+    video: 'https://www.youtube.com/watch?v=6Ejga4kJUts'
+    url: 'http://www.cranberries.com/site/'
+  }
+]
