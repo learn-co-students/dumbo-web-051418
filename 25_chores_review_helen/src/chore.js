c5e053ba755bb941a2f81697b101f2656da05270
nikymorg <nicole.morgan@gmail.com>
2018-07-05 17:50:39 -0400

    blur listener

--- 25_chores_review_helen/src/chore.js
+++ 25_chores_review_helen/src/chore.js
@@ -7,31 +7,29 @@ class Chore {
   }
 
   el(){
-    const adapter = new Adapter()
-    const div = document.createElement('div')
-    div.innerHTML = `<button class="delete-button test" data-id=${this.id}>X</button>
-      <h2>${this.title}</h2>
-      <p>${this.duration}</p>`
-    div.className = "chore-card"
-
-    const input = document.createElement('input')
-    input.value = this.priority
-    input.setAttribute("data-id", this.id)
-    input.addEventListener('blur', (event) => {
-      adapter.updateChore(this.id, event.target.value)
-    })
-
-    div.appendChild(input)
-
-    return div
-
-    // `
-    // <div class="chore-card">
-    //   <button class="delete-button test" data-id=${this.id}>X</button>
+    // const adapter = new Adapter()
+    // const div = document.createElement('div')
+    // div.innerHTML = `<button class="delete-button test" data-id=${this.id}>X</button>
     //   <h2>${this.title}</h2>
-    //   <p>${this.duration}</p>
-    //   <input type="text" data-id=${this.id} value="${this.priority}"/>
-    // </div>
-    // `
+    //   <p>${this.duration}</p>`
+    // div.className = "chore-card"
+    //
+    // const input = document.createElement('input')
+    // input.value = this.priority
+    // input.setAttribute("data-id", this.id)
+    // input.addEventListener('blur', (event) => {
+    //   adapter.updateChore(this.id, event.target.value)
+    // })
+
+    // div.appendChild(input)
+
+    return `
+    <div class="chore-card">
+      <button class="delete-button test" data-id=${this.id}>X</button>
+      <h2>${this.title}</h2>
+      <p>${this.duration}</p>
+      <input type="text" data-id=${this.id} value="${this.priority}"/>
+    </div>
+    `
   }
 }
--- 25_chores_review_helen/src/choresController.js
+++ 25_chores_review_helen/src/choresController.js
@@ -3,8 +3,9 @@ class ChoresController {
   render(chore){
     const choreList = document.querySelector('#chore-list')
     const currentChore = new Chore(chore)
-    choreList.appendChild(currentChore.el())
-
-    return currentChore
+    choreList.innerHTML += currentChore.el()
+    const input = document.querySelector(`input[data-id='${currentChore.id}']`)
+    console.log(input)
+    input.addEventListener('blur', console.log)
   }
 }
